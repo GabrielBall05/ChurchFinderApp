ae5af9f9376df4ddfb7bb25422500579e8ea154c
GabrielBall05 <gabrielb4556@gmail.com>
2023-12-17 22:45:08 -0500

    Project Creation

---
+++ ChurchFinder/app/src/main/java/com/example/churchfinder/LoginRegisterIntents/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.churchfinder.LoginRegisterIntents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.churchfinder.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
