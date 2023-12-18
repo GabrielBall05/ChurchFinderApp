a8ea1a1d020da43e1e3e28689573784f55166506
GabrielBall05 <gabrielb4556@gmail.com>
2023-12-17 23:07:03 -0500

    New Project

---
+++ ChurchApp/app/src/main/java/com/example/churchapp/Database/Database.java
@@ -0,0 +1,6 @@
+package com.example.churchapp.Database;
+
+public class Database
+{
+
+}
---
+++ ChurchApp/app/src/main/java/com/example/churchapp/Database/DatabaseVariables.java
@@ -0,0 +1,10 @@
+package com.example.churchapp.Database;
+
+public class DatabaseVariables
+{
+    public static String DB_NAME = "church_finder_database";
+    public static int DB_VERSION = 1;
+    public static String USER_TABLE = "users";
+    public static String CHURCH_TABLE = "churches";
+    public static String EVENTS_TABLE = "events";
+}
